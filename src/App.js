0e47b0d86dc76fca10fe40521e0561d5cff6d985
FreyaP <65002708+FreyaP@users.noreply.github.com>
2020-07-13 18:08:29 -0700

    Added footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,22 @@ export default function App() {
   return (
     <div className="App">
       <h1> Hello</h1>
+      <footer>
+        This app is{" "}
+        <a
+          href="https://github.com/FreyaP/final-project-react-weather-app"
+          target="_blank"
+        >
+          open sourced on Github{" "}
+        </a>
+        by{" "}
+        <a
+          href="https://www.linkedin.com/in/freya-pearse-0b6b10192/"
+          target="_blank"
+        >
+          Freya Pearse
+        </a>
+      </footer>
     </div>
   );
 }
