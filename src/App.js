351835eea68582c751913a6787e78a0fa2a83039
FreyaP <65002708+FreyaP@users.noreply.github.com>
2020-07-13 18:49:23 -0700

    Installed bootstrap

--- src/App.js
+++ src/App.js
@@ -5,25 +5,28 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1> Hello</h1>
-      <footer>
-        This app is open sourced on{" "}
-        <a
-          href="https://github.com/FreyaP/final-project-react-weather-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Github{" "}
-        </a>
-        and coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/freya-pearse-0b6b10192/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Freya Pearse
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Hello</h1>
+
+        <footer>
+          This app is open sourced on{" "}
+          <a
+            href="https://github.com/FreyaP/final-project-react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github{" "}
+          </a>
+          and coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/freya-pearse-0b6b10192/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Freya Pearse
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
