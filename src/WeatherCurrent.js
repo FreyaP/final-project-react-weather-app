42625b798bbd88fb6321da02e7a82f9eb286dad2
FreyaP <65002708+FreyaP@users.noreply.github.com>
2020-07-18 21:02:16 -0700

    Integrated weather API and loader icon

--- src/WeatherCurrent.js
+++ src/WeatherCurrent.js
@@ -1,33 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+
 import SearchEngine from "./SearchEngine";
 import "./WeatherCurrent.css";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loader from "react-loader-spinner";
 
 export default function WeatherCurrent() {
-  return (
-    <div className="Weather">
-      <h1 className="current">Current</h1>
-      <hr />
-      <h1 className="location">Vancouver, BC</h1>
-      <h5 className="date">Tues. 3rd July, 3pm</h5>
-      <div className="clear-fix weatherMain">
-        <img
-          src="https://www.iconsdb.com/icons/preview/white/rain-xxl.png"
-          alt="rain"
-        ></img>
+  const [weatherData, setWeatherdata] = useState({ ready: false });
 
-        <span className="temperature">13</span>
-        <span className="units">°C</span>
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherdata({
+      ready: true,
+      name: response.data.name,
+      description: response.data.weather[0].description,
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      feelsLike: response.data.main.feels_like,
+      country: response.data.sys.country,
+    });
+  }
 
-        <span className="temperature fahrenheit">55</span>
-        <span className="units">°F</span>
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <h1 className="current">Current</h1>
+        <hr />
+        <h1 className="location">
+          {weatherData.name}, {weatherData.country}
+        </h1>
+        <h5 className="date">Tues. 3rd July, 3pm</h5>
+        <div className="clear-fix weatherMain">
+          <img
+            src="https://www.iconsdb.com/icons/preview/white/rain-xxl.png"
+            alt="rain"
+          ></img>
+
+          <span className="temperature">
+            {Math.round(weatherData.temperature)}
+          </span>
+          <span className="units">°C</span>
+
+          <span className="temperature fahrenheit">55</span>
+          <span className="units">°F</span>
+        </div>
+        <h3 className="description text-capitalize">
+          {weatherData.description}
+        </h3>
+        <ul>
+          <li>Feels Like: {Math.round(weatherData.feelsLike)}°C</li>
+          <li>Wind: {weatherData.wind}km/h</li>
+          <li>Humidity: {weatherData.humidity}%</li>
+        </ul>
+        <SearchEngine />
       </div>
-      <h3 className="description">Rain Showers</h3>
-      <ul>
-        <li>Chance of Rain 28%</li>
-        <li>Wind 3km/h</li>
-        <li>Humidity 70%</li>
-      </ul>
-      <SearchEngine />
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "8c8f09ab6406d1fc43401acc75ad7253";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Vancouver&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+
+    return (
+      <Loader
+        type="Circles"
+        color="#F5A623"
+        height={150}
+        width={150}
+        timeout={4000} //3 secs
+      />
+    );
+  }
 }
